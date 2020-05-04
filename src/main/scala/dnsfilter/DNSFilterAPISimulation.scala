5a3d1ad7d9a29adc63e3a60d7dd1ce65d7236868
Avery <avery@eero.com>
2020-05-04 11:58:52 -0700

    skeleton

---
+++ src/main/scala/dnsfilter/DNSFilterAPISimulation.scala
@@ -0,0 +1,24 @@
+package dnsfilter
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import scala.concurrent.duration._
+
+class DNSFilterAPISimulation extends Simulation {
+
+  val baseURL = "http://example.com"
+  override def httpConf = super.httpConf.header("MyHeader", "MyValue")
+
+  spec {
+    http("Example index.html test")
+      .get("/index.html")
+      .check(h1.exists)
+  }
+
+}
+
+object GatlingFunSpecExampleIT {
+
+  def h1 = css("h1")
+
+}
